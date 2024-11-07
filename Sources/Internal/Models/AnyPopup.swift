f7d53132a1c4e41c17c0dabf94b00dec02f51c8d
Tomasz Kurylik <fulcrumone@icloud.com>
2024-11-07 21:53:41 +0100

    1

--- Sources/Internal/Models/AnyPopup.swift
+++ Sources/Internal/Models/AnyPopup.swift
@@ -54,7 +54,7 @@ extension AnyPopup {
     func settingDragHeight(_ newDragHeight: CGFloat?) -> AnyPopup { updatingPopup { $0.dragHeight = newDragHeight }}
     func settingEnvironmentObject(_ environmentObject: some ObservableObject) -> AnyPopup { updatingPopup { $0._body = .init(_body.environmentObject(environmentObject)) }}
 }
-private extension AnyPopup {
+extension AnyPopup {
     func updatingPopup(_ customBuilder: (inout AnyPopup) -> ()) -> AnyPopup {
         var popup = self
         customBuilder(&popup)
