a763d3b17af04c9bb15e9ed44f0c03a5312f00b1
Tomasz Kurylik <fulcrumone@icloud.com>
2024-11-07 12:38:57 +0100

    1

--- Sources/Internal/View Models/ViewModel+CentreStack.swift
+++ Sources/Internal/View Models/ViewModel+CentreStack.swift
@@ -111,7 +111,7 @@ private extension VM.CentreStack {
 
 // MARK: Methods
 extension VM.CentreStack {
-    func t_calculateHeight(heightCandidate: CGFloat) -> CGFloat { recalculatePopupHeight(heightCandidate) }
+    func t_calculateHeight(heightCandidate: CGFloat) -> CGFloat { min(heightCandidate, calculateLargeScreenHeight()) }
     func t_calculatePopupPadding() -> EdgeInsets { calculatePopupPadding() }
     func t_calculateCornerRadius() -> [PopupAlignment: CGFloat] { calculateCornerRadius() }
     func t_calculateOpacity(for popup: AnyPopup) -> CGFloat { calculateOpacity(for: popup) }
