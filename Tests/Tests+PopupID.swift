b2af4d508571198cabc8166d3fe0d802f2c2dd29
Tomasz Kurylik <fulcrumone@icloud.com>
2024-11-14 22:26:02 +0100

    1

--- Tests/Tests+PopupID.swift
+++ Tests/Tests+PopupID.swift
@@ -23,20 +23,20 @@ import SwiftUI
 
 // MARK: Create ID
 extension PopupIDTests {
-    func test_createPopupID_1() {
+    func test_createPopupID_1() async {
         let dateString = String(describing: Date())
 
-        let popupID = PopupID.create(from: TestTopPopup.self)
+        let popupID = await PopupID(TestTopPopup.self)
         let idComponents = popupID.rawValue.components(separatedBy: "/{}/")
 
         XCTAssertEqual(idComponents.count, 2)
         XCTAssertEqual(idComponents[0], "TestTopPopup")
         XCTAssertEqual(idComponents[1], dateString)
     }
-    func test_createPopupID_2() {
+    func test_createPopupID_2() async {
         let dateString = String(describing: Date())
 
-        let popupID = PopupID.create(from: TestCenterPopup.self)
+        let popupID = await PopupID(TestCenterPopup.self)
         let idComponents = popupID.rawValue.components(separatedBy: "/{}/")
 
         XCTAssertEqual(idComponents.count, 2)
@@ -47,38 +47,38 @@ extension PopupIDTests {
 
 // MARK: Is Same Type
 extension PopupIDTests {
-    func test_isSameType_1() {
-        let popupID1 = PopupID.create(from: TestTopPopup.self),
-            popupID2 = PopupID.create(from: TestBottomPopup.self)
+    func test_isSameType_1() async {
+        let popupID1 = await PopupID(TestTopPopup.self),
+            popupID2 = await PopupID(TestBottomPopup.self)
 
         let result = popupID1.isSameType(as: popupID2)
         XCTAssertEqual(result, false)
     }
-    func test_isSameType_2() {
-        let popupID1 = PopupID.create(from: TestTopPopup.self),
+    func test_isSameType_2() async {
+        let popupID1 = await PopupID(TestTopPopup.self),
             popupID2 = "TestTopPopup"
 
         let result = popupID1.isSameType(as: popupID2)
         XCTAssertEqual(result, true)
     }
-    func test_isSameType_3() {
-        let popupID1 = PopupID.create(from: "2137"),
+    func test_isSameType_3() async {
+        let popupID1 = await PopupID("2137"),
             popupID2 = "2137"
 
         let result = popupID1.isSameType(as: popupID2)
         XCTAssertEqual(result, true)
     }
-    func test_isSameType_4() {
-        let popupID1 = AnyPopup(TestTopPopup().setCustomID("2137")).id,
-            popupID2 = AnyPopup(TestTopPopup()).id
+    func test_isSameType_4() async {
+        let popupID1 = await AnyPopup(TestTopPopup().setCustomID("2137")).id,
+            popupID2 = await AnyPopup(TestTopPopup()).id
 
         let result = popupID1.isSameType(as: popupID2)
         XCTAssertEqual(result, false)
     }
     func test_isSameType_5() async {
-        let popupID1 = PopupID.create(from: TestTopPopup.self)
+        let popupID1 = await PopupID(TestTopPopup.self)
         await Task.sleep(seconds: 1)
-        let popupID2 = PopupID.create(from: TestTopPopup.self)
+        let popupID2 = await PopupID(TestTopPopup.self)
 
         let result = popupID1.isSameType(as: popupID2)
         XCTAssertEqual(result, true)
@@ -87,9 +87,9 @@ extension PopupIDTests {
 
 // MARK: Is Same
 extension PopupIDTests {
-    func test_isSame_1() {
-        let popupID = PopupID.create(from: TestTopPopup.self),
-            popup = AnyPopup(TestCenterPopup())
+    func test_isSame_1() async {
+        let popupID = await PopupID(TestTopPopup.self),
+            popup = await AnyPopup(TestCenterPopup())
 
         let result = popupID.isSame(as: popup)
         XCTAssertEqual(result, false)
